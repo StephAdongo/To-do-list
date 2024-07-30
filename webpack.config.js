6eca2eb0d7ba223f266bc0bc88aced09ab505327
StephAdongo <stephanie@kabarak.ac.ke>
2024-07-30 20:18:45 +0300

    add .

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require ('path');
+const htmlWebpackPlugin = require ("html-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename:'[name].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool:'inline-source-map',
+    plugins: [
+        new htmlWebpackPlugin({
+            template: "./src/index.html",
+            title:"To-do-list"
+        }),
+    ],
+    module: {
+        rules:[
+            {
+                test:/\.css$/i,
+                use:['style-loader','css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type:'asset/resources',
+            },
+        ],
+    }
+};
